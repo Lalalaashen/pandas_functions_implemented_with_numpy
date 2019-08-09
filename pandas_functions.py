7b26da63b2079590915b842c13e669b738042a9b
Eason <47597085+Lalalaashen@users.noreply.github.com>
2019-08-09 11:06:06 +0800

    Update pandas_functions.py

--- pandas_functions.py
+++ pandas_functions.py
@@ -2,51 +2,38 @@
 # @Time     : 2019.07
 # @Author   : Eason
 # @FileName : pandas_functions.py
-import numpy as np
 
 
-# pandas->diff
-def pct_change(xs, n, fill_value=np.nan) -> np.ndarray:
-    '''
-    >>> x = np.arange(1, 10, 1)
-    >>> pct_change(x, 1)
-    '''
-    return xs / shift(xs, n, fill_value) - 1
+# 实现类似pandas的shift
+def shift_(xs, n, fill_value=None):
+    import numpy as np
+
+    fill_value = np.nan if fill_value is None else fill_value
+    return np.r_[np.full(n, fill_value), xs[:-n]] if n >= 0 else np.r_[xs[-n:], np.full(-n, fill_value)]
 
 
-# pandas->diff
-def diff(xs, n, fill_value=np.nan) -> np.ndarray:
-    '''
-    >>> x = np.arange(1, 10, 1)
-    >>> diff(x, 1)
-    '''
-    return xs - shift(xs, n, fill_value)
+# 实现类似于pandas的rolling
+def rolling_(arr, window, func1d, fill_value=None, fill_before=True):
+    import numpy as np
 
-
-# pandas->shift
-def shift(xs, n, fill_value=np.nan) -> np.ndarray:
-    '''
-    >>> x = np.arange(1, 10, 1)
-    >>> shift(x, 1)
-    >>> shift(x, -1)
-    '''
-    if n >= 0:
-        return np.r_[np.full(n, fill_value), xs[:-n]]
-    else:
-        return np.r_[xs[-n:], np.full(-n, fill_value)]
-
-
-def rolling(arr, window, np_func, fill_value=np.nan, forward=True) -> np.ndarray:
-    '''
-    >>> x = np.arange(1, 10, 1)
-    >>> rolling(x, 2, np.min)
-    >>> rolling(x, 2, np.max)
-    >>> rolling(x, 2, np.max, forward=False)
-    '''
     shape = arr.shape[:-1] + (arr.shape[-1] - window + 1, window)
     strides = arr.strides + (arr.strides[-1],)
     roll = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=strides)
-    if forward:
-        return np.append(np.zeros(window - 1) + fill_value, np_func(roll, axis=1))
+
+    roll_value = np.apply_along_axis(func1d=func1d, axis=1, arr=roll)
+
+    fill_value = np.nan if fill_value is None else fill_value
+    if len(roll_value.shape) > 1:
+        fill_value = np.zeros((window - 1, roll_value.shape[1])) + fill_value
+        return np.vstack((fill_value, roll_value)) if fill_before else np.vstack((roll_value, fill_value))
     else:
-        return np.append(np_func(roll, axis=1), np.zeros(window - 1) + fill_value)
+        fill_value = np.zeros(window - 1) + fill_value
+        return np.append(fill_value, roll_value) if fill_before else np.append(roll_value, fill_value)
+
+
+# pandas->diff_
+def pct_change_(xs, n, fill_value=None): return xs / shift_(xs, n, fill_value) - 1
+
+
+# pandas->diff_
+def diff_(xs, n, fill_value=None): return xs - shift_(xs, n, fill_value)
